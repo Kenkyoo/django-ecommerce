e0a55ee929285694def1e193f58712241323859d
Franco <frfantusatti@protonmail.com>
2026-06-25 08:12:46 -0300

    add templates index.html

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,11 +51,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'store', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
