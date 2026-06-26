a1eaebe03fb5829be43d34bb55f01a8b0ddd2f43
Franco <frfantusatti@protonmail.com>
2026-06-26 04:44:40 -0300

    add cart.html

--- ecommerce/store/models/__init_.py
+++ ecommerce/store/models/__init_.py
@@ -2,3 +2,4 @@
 from .products import Products
 from .category import Category
 from .customer import Customer
+form .orders import Order
--- ecommerce/store/views/cart.py
+++ ecommerce/store/views/cart.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from django.views import View
 from store.models.products import Products
 
-
 class Cart(View):
     def get(self, request):
-        cart = request.session.get('cart')
+        cart = request.session.get('cart', {})
+        products = []
+        total = 0
 
-        if not cart:
-            products = []
-        else:
-            products = Products.get_products_by_id(list(cart.keys()))
+        if cart:
+            product_list = Products.get_products_by_id(list(cart.keys()))
+            for product in product_list:
+                quantity = cart.get(str(product.id), 0)
+                subtotal = product.price * quantity
+                total += subtotal
+                products.append({
+                    'product': product,
+                    'quantity': quantity,
+                    'subtotal': subtotal,
+                })
 
         return render(request, 'cart.html', {
-            'products': products
+            'products': products,
+            'total': total,
         })
